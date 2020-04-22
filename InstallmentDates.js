a38c8aa5e8788a9a83d07b9cb4d4ff94a579ba35
Natanael Simões <natanael.simoes@logicamente.info>
2020-04-22 00:25:16 -0400

    initial

---
+++ InstallmentDates.js
@@ -0,0 +1,54 @@
+let _startDate, _payDay, _installments;
+
+function isInt(value) {
+    var x;
+    return isNaN(value) ? !1 : (x = parseFloat(value), (0 | x) === x);
+}
+
+function lastDate(month, year) {
+    return (new Date(year, month + 1, 0)).getDate();
+}
+
+function validateParams(startDate, dayToPay, installments) {
+    if (typeof startDate === 'number' && startDate >= 0)
+        _startDate = new Date(startDate);
+    else if (typeof startDate === 'object'
+        && startDate.constructor.name === 'Date')
+        _startDate = startDate;
+    else throw 'Start date is not a valid date';
+    if (!isInt(dayToPay))
+        throw 'Day to pay needs to be an integer';
+    _payDay = parseInt(dayToPay);
+    if (_payDay < 1 || _payDay > 31)
+        throw 'Day to pay needs to be between 1 and 31 (inclusive)';
+    if (!isInt(installments))
+        throw 'Installments needs to be an integer';
+    _installments = parseInt(installments);
+    if (_installments < 1)
+        throw 'Installments needs to be greater or equal to 1';
+}
+
+function InstallmentDates(startDate, payDay, installments) {
+    let lastDay, dates = [];
+    validateParams(startDate, payDay, installments);
+    console.log('Start date: ', _startDate);
+    if (_startDate.getDate() > _payDay) {
+        lastDay = lastDate(_startDate.getMonth() + 1, _startDate.getFullYear());
+        _startDate.setDate(_payDay <= lastDay ? _payDay : lastDay);
+        _startDate.setMonth(_startDate.getMonth() + 1);
+    } else {
+        lastDay = lastDate(_startDate.getMonth(), _startDate.getFullYear());
+        _startDate.setDate(_payDay <= lastDay ? _payDay : lastDay);
+    }
+    for (let i = 0; i < installments; i++) {
+        const current = new Date(_startDate.getTime());
+        dates.push(current);
+        lastDay = lastDate(_startDate.getMonth() + 1, _startDate.getFullYear());
+        _startDate.setDate(1);
+        _startDate.setMonth(_startDate.getMonth() + 1);
+        _startDate.setDate(_payDay <= lastDay ? _payDay : lastDay);
+    }
+    return dates;
+}
+
+module.exports = InstallmentDates;
---
+++ tests.spec.js
@@ -0,0 +1,33 @@
+const PaymentDate = require('./InstallmentDates');
+
+describe("Payment date generator", () => {
+    it("day to pay greater than start date day", (done) => {
+        const dates = PaymentDate(new Date(2020, 3, 5), 20, 3);
+        expect(typeof dates).toBe('object');
+        expect(dates.length).toBe(3);
+        expect(new Intl.DateTimeFormat('en-US').format(dates[0])).toBe('4/20/2020');
+        expect(new Intl.DateTimeFormat('en-US').format(dates[1])).toBe('5/20/2020');
+        expect(new Intl.DateTimeFormat('en-US').format(dates[2])).toBe('6/20/2020');
+        done();
+    });
+
+    it("day to pay less than start date day", (done) => {
+        const dates = PaymentDate(new Date(2020, 3, 20), 5, 3);
+        expect(typeof dates).toBe('object');
+        expect(dates.length).toBe(3);
+        expect(new Intl.DateTimeFormat('en-US').format(dates[0])).toBe('5/5/2020');
+        expect(new Intl.DateTimeFormat('en-US').format(dates[1])).toBe('6/5/2020');
+        expect(new Intl.DateTimeFormat('en-US').format(dates[2])).toBe('7/5/2020');
+        done();
+    });
+
+    it("day to pay does not exists next month", (done) => {
+        const dates = PaymentDate(new Date(2020, 1, 1), 31, 3);
+        expect(typeof dates).toBe('object');
+        expect(dates.length).toBe(3);
+        expect(new Intl.DateTimeFormat('en-US').format(dates[0])).toBe('2/29/2020');
+        expect(new Intl.DateTimeFormat('en-US').format(dates[1])).toBe('3/31/2020');
+        expect(new Intl.DateTimeFormat('en-US').format(dates[2])).toBe('4/30/2020');
+        done();
+    });
+});
